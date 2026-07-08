96b0f1eb596c2086ba01771dd5bae255d450f6cd
Kavitha0703 <nasikakavitha@gmail.com>
2026-07-08 14:55:37 +0530

    chore: update PWA assets and manifest configuration
    Standardize icon references and update manifest metadata to improve PWA installation and branding consistency.

--- android/app/src/main/assets/public/sw.js
+++ android/app/src/main/assets/public/sw.js
@@ -1,1 +1,119 @@
-if(!self.define){let e,s={};const i=(i,n)=>(i=new URL(i+".js",n).href,s[i]||new Promise(s=>{if("document"in self){const e=document.createElement("script");e.src=i,e.onload=s,document.head.appendChild(e)}else e=i,importScripts(i),s()}).then(()=>{let e=s[i];if(!e)throw new Error(`Module ${i} didn’t register its module`);return e}));self.define=(n,r)=>{const l=e||("document"in self?document.currentScript.src:"")||location.href;if(s[l])return;let o={};const t=e=>i(e,l),u={module:{uri:l},exports:o,require:t};s[l]=Promise.all(n.map(e=>u[e]||t(e))).then(e=>(r(...e),o))}}define(["./workbox-9c191d2f"],function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"registerSW.js",revision:"1872c500de691dce40960bb85481de07"},{url:"index.html",revision:"2180b3ea2a134ea8f0a4b88e91742b11"},{url:"assets/purify.es-CPrKKeqe.js",revision:null},{url:"assets/pdf-NkhGRooA.js",revision:null},{url:"assets/index.es-CTfZaU1D.js",revision:null},{url:"assets/index-DOpDTNfF.css",revision:null},{url:"assets/index-CSHDYKPa.js",revision:null},{url:"assets/index-BUOvqE4x.js",revision:null},{url:"assets/html2canvas.esm-QH1iLAAe.js",revision:null},{url:"icons/icon-192.png",revision:"8170009f1ec7607852829203e8cb69f4"},{url:"icons/icon-512.png",revision:"e2d70e869ff1e91469a8ae4d254e8891"},{url:"manifest.webmanifest",revision:"4036eaee96114d483026a46d6d00b9f8"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))});
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "1872c500de691dce40960bb85481de07"
+  }, {
+    "url": "index.html",
+    "revision": "88335ee9092ef2bbbecaed79be9a201f"
+  }, {
+    "url": "assets/purify.es-CPrKKeqe.js",
+    "revision": null
+  }, {
+    "url": "assets/pdf-NkhGRooA.js",
+    "revision": null
+  }, {
+    "url": "assets/index.es-Dt4FEmX3.js",
+    "revision": null
+  }, {
+    "url": "assets/index-l4YUBF5U.js",
+    "revision": null
+  }, {
+    "url": "assets/index-DOpDTNfF.css",
+    "revision": null
+  }, {
+    "url": "assets/index-B8h82Ppf.js",
+    "revision": null
+  }, {
+    "url": "assets/html2canvas.esm-QH1iLAAe.js",
+    "revision": null
+  }, {
+    "url": "logo-192.png",
+    "revision": "a5215afd69c90b6850188b560f187935"
+  }, {
+    "url": "logo-512.png",
+    "revision": "5546b369a7598632244fe1f496c973e0"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "e2bfcd587596c6273215b3c3e8590f6e"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
